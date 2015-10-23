0ddfae4f4a4971b47cefc9392c5460747a6f0672
Daniel Buchner <danieljb2@gmail.com>
2015-10-23 16:07:46 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,23 +1,49 @@
-(function(){  
+(function(){
 
-  // Create your component here
-  // http://x-tags.org/docs
-  
-  xtag.register('x-stub', {
+  function releaseContainer(box){
+    if (box.xtag.container) {
+      box.xtag.container.removeAttribute('x-peekbox-container');
+      box.xtag.container = null;
+    }
+  }
+
+  xtag.register('x-peekbox', {
     lifecycle: {
-      created: function() {},
-      inserted: function() {},
-      removed: function() {},
-      attributeChanged: function() {}
-    }, 
-    events: { 
-    
+      inserted: function(){
+        releaseContainer(this);
+        if (this.parentNode) {
+          (this.xtag.container = this.parentNode).setAttribute('x-peekbox-container', '');
+        }
+      },
+      removed: function(){
+        releaseContainer(this);
+      }
+    },
+    events: {
+      transitionend: function(e){
+        if (e.target == this){
+          xtag.fireEvent(this, this.hasAttribute('x-peekbox-show') ? 'show' : 'hide');
+        }
+      }
     },
     accessors: {
-      
-    }, 
+      edge: {
+        attribute: {},
+        set: function(){
+          xtag.skipTransition(this);
+        }
+      }
+    },
     methods: {
-      
+      show: function(){
+        this.setAttribute('x-peekbox-show', '');
+      },
+      hide: function(){
+        this.removeAttribute('x-peekbox-show');
+      },
+      toggle: function(){
+        this.hasAttribute('x-peekbox-show') ? this.hide() : this.show();
+      }
     }
   });
 
